43a41120d9fb19d42929d1df524499eeefe07a91
Amypetua <anachusichiamaka32@gmail.com>
2023-05-03 15:20:30 -0700

    Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,17 @@
-#!/usr/bin/env ruby
-puts ARGV[0].scan(/\[From:(.*?)\] \[To:(.*?)\] \[flags:(.*?)\]/).join(",")
+#!/usr/bin/env ruby 
+
+input_str = ARGV[0]
+  
+  match_data = input_str.match(/\[From: (.*?)\] \[To: (.*?)\] \[flags: (.*?)\]/)
+
+  if match_data
+    sender = match_data[1]
+    receiver = match_data[2]
+    flags = match_dats[3]
+
+    puts "#{sender}, #{receiver}, #{flags}"
+  else
+    puts "No match found."
+  end
 
 
