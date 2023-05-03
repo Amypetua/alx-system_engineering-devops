735e95e67f6d489b6a6241b245a44c8cb92baa17
Amypetua <anachusichiamaka32@gmail.com>
2023-05-03 15:29:24 -0700

    Text me

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,17 +1,3 @@
 #!/usr/bin/env ruby 
 
-input_str = ARGV[0]
-  
-  match_data = input_str.match(/\[From: (.*?)\] \[To: (.*?)\] \[flags: (.*?)\]/)
-
-  if match_data
-    sender = match_data[1]
-    receiver = match_data[2]
-    flags = match_dats[3]
-
-    puts "#{sender}, #{receiver}, #{flags}"
-  else
-    puts "No match found."
-  end
-
-
+puts ARGV[0].scan(/\[From:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
