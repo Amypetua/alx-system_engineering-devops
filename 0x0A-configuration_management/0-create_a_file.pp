0e23b27541835de2b14c3fc96aa0fbdcbfa77aa3
Amypetua <anachusichiamaka32@gmail.com>
2023-06-26 13:23:57 -0700

    Puppet magic

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# Create a file in /tmp using puppet.
+# Puppet manifests that create a file in /tmp using puppet.
 file { '/tmp/school':
-  ensure => present,
-  mode => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+  ensure  => present,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,8 @@
+# Puppet manifests that create a manifest that kills a process named killmenow
+exec { 'killing a process using pkill':
+  command  => 'pkill 9 killmenow',
+  path     => '/usr/bin:/bin',
+  onlyif   => 'pgrep killmenow',
+  provider => shell,
+
+}
