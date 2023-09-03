029688874cf68389d0407b6c122f6988d9487ec5
Amypetua <anachusichiamaka32@gmail.com>
2023-09-03 08:07:13 +0000

    Kill a process

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,7 @@
 # A puppet manifest that kills a process called killmenow.
-exec { ''kill a process using pkill':
+exec { 'killing a process using pkill':
   command  => 'pkill -9 killmenow',
   path     => 'usr/bin:/bin',
   onlyif   => 'pgrep killmenow',
-
   provider => shell,
 }
